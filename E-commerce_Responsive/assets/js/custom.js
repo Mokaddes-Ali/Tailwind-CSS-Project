7151a6d143313210f1c081345b2dca641ac996a4
Mokaddes Ali <mokaddes.ru200@gmail.com>
2025-07-20 20:48:12 +0600

    add tab commit

---
+++ E-commerce_Responsive/assets/js/custom.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+  const $underline = $('#underline');
+  const $tabItems = $('.tab-item');
+  const $tabContents = $('.tab-content');
+
+  // দ্বিতীয় ট্যাব active করো (index 1)
+  $tabItems.eq(1).addClass('active').css({ color: 'black', 'font-weight': '500' });
+  $tabContents.hide().eq(1).show();
+  setUnderline($tabItems.eq(1));
+
+  // ট্যাব ক্লিক ইভেন্ট
+  $tabItems.click(function () {
+    $tabItems.removeClass('active').css({ color: 'rgba(0,0,0,0.6)', 'font-weight': '400' });
+    $(this).addClass('active').css({ color: 'black', 'font-weight': '500' });
+
+    const index = $(this).index();
+
+    $tabContents.hide().eq(index).show();
+    setUnderline($(this));
+  });
+
+  function setUnderline($tab) {
+    const left = $tab.position().left;
+    const width = $tab.outerWidth();
+    $underline.css({ left: left + 'px', width: width + 'px' });
+  }
+});
+
