221b48df2a9c5a675ec967194d79b7d336aabce1
Mokaddes Ali <mokaddes.ru2000@gmail.com>
2025-08-24 13:02:21 +0600

    add hospital apoinrt ment system project complete service paghe and add others necesary data commit

---
+++ Doctor-Appoinment_Project/common/footer.php
@@ -0,0 +1,14 @@
+<footer class="w-[1366px] bg-[#1F2B6C]  mx-auto px-[187px] pt-[80px] flex flex-col items-center gap-5">
+
+<div class="grid grid-cols-3 gap-4">
+    <!-- first row logo and description-->
+     <div class="logo-description flex flex-col gap-4">
+    <div class="w-[193px] h-[25px]">
+        <img src="../src/images/footerlogo.png" alt="Logo" class="w-full h-full">
+    </div>
+        <p class="text-[#FCFEFE] font-worksans text-[18px] not-italic font-normal leading-[140%]">Leading the Way in Medical
+Execellence, Trusted Care.</p>
+    </div>
+</div>
+
+</footer>
